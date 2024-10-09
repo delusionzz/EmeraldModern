bfc89a18b6e404ea49cf61684a779ae027365a6b
illusionTBA <illusionztba@gmail.com>
2024-10-09 14:51:51 -0400

    update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,15 +4,27 @@ importScripts("/uv/uv.bundle.js");
 importScripts("/uv/uv.sw.js");
 importScripts("/uv/uv.config.js");
 
+importScripts(
+  "/scram/scramjet.wasm.js",
+  "/scram/scramjet.codecs.js",
+  "/scram/scramjet.shared.js",
+  "/scram/scramjet.worker.js"
+);
+
 uv = new UVServiceWorker();
+const scramjet = new ScramjetServiceWorker();
+
+async function handleRequest(event) {
+  await scramjet.loadConfig();
+
+  if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+    return await uv.fetch(event);
+  } else if (scramjet.route(event)) {
+    return scramjet.fetch(event);
+  }
+  return await fetch(event.request);
+}
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
-        return await uv.fetch(event);
-      }
-      return await fetch(event.request);
-    })()
-  );
+  event.respondWith(handleRequest(event));
 });
