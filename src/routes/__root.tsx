c8c8a5a8b1e1bd4e24b5373f700d75a35ce4b50d
illusions <illusionztba@gmail.com>
2024-11-01 15:44:27 +0000

    Fix: Tanstack devtools showing in prod

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,17 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import React, { Suspense } from 'react'
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
@@ -14,7 +26,9 @@ export const Route = createRootRoute({
       </div>
       <hr /> */}
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 })
